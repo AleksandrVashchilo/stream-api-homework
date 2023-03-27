e908adcda54f4910bf8971523fa9783212fe2768
AleksandrVashchilo <suszycki.alexander@gmail.com>
2023-03-27 18:44:48 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MovieCatalog catalog = new MovieCatalog();
+
+        catalog.addMovie("Star Wars: Episode V - The Empire Strikes Back", 1980, "Adventure", 8.7);
+        catalog.addMovie("The Matrix", 1999, "Sci=fi", 8.6);
+        catalog.addMovie("Terminator 2: Judment Day", 1991, "Action", 8.1);
+
+        System.out.println();
+        System.out.println("--------------------------------");
+        System.out.println(catalog.getMovies());
     }
 }
